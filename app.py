3d9b12712f215730744985a16c8c634fc6be09d5
dev_jenniem <96963955+jmauch13@users.noreply.github.com>
2025-05-04 14:28:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ REPLICATE_API_TOKEN = "public_W23MT9S845nFTBpibnT3E5xWawMU"
 MODEL_VERSION = "cc3fdd6f084d0cbe255191bbab6d4793a1e0be1adab747d25b7e3c52b24c435b"
 
 
+
 @app.route("/clone", methods=["POST"])
 def clone_face():
     data = request.json
